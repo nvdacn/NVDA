de613063dd94c52394dcb7f73335df3dfe85a597
Leonard de Ruijter <leonard@babbage.com>
2018-07-19 17:19:57 +0200

    Attempt for screen curtain based on windows magnification api

--- source/vision/screenMask.py
+++ source/vision/screenMask.py
@@ -4,48 +4,35 @@
 #See the file COPYING for more details.
 #Copyright (C) 2018 NV Access Limited
 
-"""Screen mask/curtain implementation based on a wx overlay window."""
+"""Screen mask/curtain implementation based on the windows magnification API."""
 
 from . import ColorEnhancer, ColorTransformation
-import wx
-import gui
-import api
-# NVDA's built-in colors module is compatible with wx colors
-import colors.predefined
+import winMagnification
+from ctypes import byref
+
+TRANSFORM_BLACK = winMagnification.MAGCOLOREFFECT()
+TRANSFORM_BLACK.transform[4][4] = 1.0
+TRANSFORM_DEFAULT = winMagnification.MAGCOLOREFFECT()
+TRANSFORM_DEFAULT.transform[0][0] = 1.0
+TRANSFORM_DEFAULT.transform[1][1] = 1.0
+TRANSFORM_DEFAULT.transform[2][2] = 1.0
+TRANSFORM_DEFAULT.transform[3][3] = 1.0
+TRANSFORM_DEFAULT.transform[4][4] = 1.0
 
 class ScreenMask(ColorEnhancer):
 	name = "screenMask"
 	availableTransformations = (
-		ColorTransformation("black", colors.predefined.black.name, colors.predefined.black)
+		ColorTransformation("black", _("black screen"), TRANSFORM_BLACK)
 	)
 
 	def __init__(self, *roles):
-		self.window = None
-		self.timer = None
+		winMagnification.Initialize()
 		super(ScreenMask, self).__init__(*roles)
 
 	def initializeColorEnhancer(self):
 		super(ScreenMask, self).initializeColorEnhancer()
-		self.window = wx.Frame(parent=gui.mainFrame, id=wx.ID_ANY, pos=(0,0), size=api.getDesktopObject().location[2:], style=wx.FRAME_NO_TASKBAR | wx.STAY_ON_TOP)
-		self.window.SetBackgroundColour(wx.BLUE)
-		self.timer = wx.PyTimer(self.refresh)
-		self.timer.Start(16)
-		self.window.Disable()
-		self.window.Show()
+		winMagnification.SetFullscreenColorEffect(byref(TRANSFORM_BLACK))
 
 	def terminateColorEnhancer(self):
-		if self.timer:
-			self.timer.Stop()
-			self.timer = None
-		if self.window:
-			self.window.Hide()
-			self.window.Destroy()
-			self.window = None
+		winMagnification.SetFullscreenColorEffect(byref(TRANSFORM_DEFAULT))
 		super(ScreenMask, self).terminateColorEnhancer()
-
-	def refresh(self):
-		"""Refresh wrapper that gracefully handles the case that the mainframe gets destroyed."""
-		if self.window:
-			self.window.Refresh()
-		else:
-			self.timer.Stop()
